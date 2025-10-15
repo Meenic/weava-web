ed217503cbf604f684a6036b63121bd17b538b96
Meenic <batuhanozata80@gmail.com>
2025-10-15 18:43:37 +0300

    refactor: remove StoryShowcase component from Home page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,11 +1,9 @@
 import { HeroSection } from "@/components/marketing/hero-section";
-import { StoryShowcase } from "@/components/marketing/story-showcase";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-      <StoryShowcase />
     </>
   );
 }
