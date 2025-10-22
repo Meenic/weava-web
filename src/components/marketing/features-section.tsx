bb66d41b1b8a97c1e22f4c06c4851cae5c0d65d8
Meenic <batuhanozata80@gmail.com>
2025-10-22 13:23:13 +0300

    feat: add FeaturesSection component

---
+++ src/components/marketing/features-section.tsx
@@ -0,0 +1,7 @@
+export function FeaturesSection() {
+  return (
+    <section className="flex items-center justify-center px-4 py-16 sm:py-20 md:py-24">
+      <h1 className="">Some Features</h1>
+    </section>
+  );
+}
