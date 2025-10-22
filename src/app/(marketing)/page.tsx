3e086422ee4e60ee847213dc5ee9cd974defbbc5
Meenic <batuhanozata80@gmail.com>
2025-10-22 13:22:35 +0300

    feat: add FeaturesSection to Home component

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,9 +1,11 @@
+import { FeaturesSection } from "@/components/marketing/features-section";
 import { HeroSection } from "@/components/marketing/hero-section";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <FeaturesSection />
     </>
   );
 }
