4749ab1d4e6f2ad66a11ed3da087b1ef38f05340
Meenic <batuhanozata80@gmail.com>
2025-10-22 13:18:31 +0300

    feat: update database configuration and add authentication schema and migrations

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,11 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "@/db"; // your drizzle instance
+import { db } from "@/drizzle/db";
 import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
-    provider: "pg", // or "mysql", "sqlite"
+    provider: "pg",
   }),
   emailAndPassword: {
     enabled: true,
